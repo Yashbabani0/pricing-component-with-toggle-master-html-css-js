24ac6e66a7d74aa6d94bb6744b361a12bc6a37d1
Yashbabani0 <yashbabani09@gmail.com>
2024-04-19 16:53:13 +0530

    completed

--- script.js
+++ script.js
@@ -1,6 +1,3 @@
-// Add this script to your script.js file
-
-// Function to toggle between monthly and yearly prices
 function togglePrice() {
   // Get all elements with class 'month-price'
   var monthlyPrices = document.querySelectorAll(".month-price");
@@ -9,31 +6,28 @@ function togglePrice() {
   // Get the toggle switch element
   var toggleSwitch = document.querySelector('.switch input[type="checkbox"]');
 
-  // Iterate through each monthly price element
   monthlyPrices.forEach(function (element) {
-    // If toggle switch is checked, show monthly prices, else hide them
     if (toggleSwitch.checked) {
-      element.style.display = "inline"; // Show monthly prices
+      element.style.display = "inline";
     } else {
-      element.style.display = "none"; // Hide monthly prices
+      element.style.display = "none"; 
     }
   });
 
-  // Iterate through each yearly price element
   yearlyPrices.forEach(function (element) {
-    // If toggle switch is checked, hide yearly prices, else show them
     if (toggleSwitch.checked) {
-      element.style.display = "none"; // Hide yearly prices
+      element.style.display = "none";
     } else {
-      element.style.display = "inline"; // Show yearly prices
+      element.style.display = "inline"; 
     }
   });
 }
 
-// Attach the togglePrice function to the change event of the toggle switch
 document
   .querySelector('.switch input[type="checkbox"]')
   .addEventListener("change", togglePrice);
 
-// Call togglePrice initially to set the initial state based on the toggle switch position
+
 togglePrice();
+
+
